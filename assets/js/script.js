c9b6f76df0b107e86abbdfbb6911520c0105fe2e
Marcio Carvalho <marcio.lgc@outlook.com>
2026-04-05 15:59:37 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
-console.log("Site carregado com sucesso.");
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
